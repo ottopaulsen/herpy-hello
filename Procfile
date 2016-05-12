25671a66976e4c16221d2b0612765a12e9126249
Otto Paulsen <ottpau@gmail.com>
2016-05-12 10:08:53 +0200

    Cooked pyramid setup

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python helloworld.py
+web: ./run.sh
--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
 
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 from datetime import datetime
 import os
+from paste.deploy import loadapp
+from waitress import serve
 
 
 def hello_world(request):
@@ -14,8 +15,11 @@ if __name__ == '__main__':
     config = Configurator()
     config.add_route('hello', '/hello/{name}')
     config.add_view(hello_world, route_name='hello')
-    app = config.make_wsgi_app()
-    port = int(os.environ['PORT'])
-    server = make_server('ottpau-hello.herokuapp.com', port, app)
+    port = int(os.environ.get('PORT', 5000))
+    # app = config.make_wsgi_app()
+    app = loadapp('config:production.ini', relative_to='.')
     print('{} Starting server on port {}'.format(datetime.now(), port))
-    server.serve_forever()
+    serve(app, host='0.0.0.0', port=port)
+
+    # server = make_server('0.0.0.0', port, app)
+    # server.serve_forever()
